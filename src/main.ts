e3d09e45949e370198e90f17992b56bab0a80624
Kleber Silva <kleber.swf@gmail.com>
2022-06-13 14:21:24 -0300

    Added parameters model

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,16 @@ import { Game } from './game';
 import { KanaModel } from './model';
 import { ParameterSelector } from './parameter-selector';
 
-let content: KanaModel[];
 const parameters = new ParameterSelector();
 const game = new Game();
 
 document.querySelector('#start-button')!
 	.addEventListener('click', () => {
-		if (parameters.canStart) game.start(content, ['reads', 'tip', 'writes']);
+		if (parameters.canStart) game.start(parameters.data, ['reads', 'tip', 'writes']);
 		else showError('Select at least two characters');
 	});
 
 Promise.all(([hiragana] as any[]).map(e => fetch(e)))
 	.then(responses => Promise.all(responses.filter(e => e.ok).map(e => e.json())))
-	.then(jsons => {
-		content = jsons;
-		parameters.setup(document.querySelector('#selector')!, jsons);
-	})
+	.then(contents => parameters.setup(document.querySelector('#parameters')!, contents))
 	.catch(console.error);
-
-
