972897f8e3aa8ee5f6e6582abd7cc94c2ef27ba8
Kleber Silva <kleber.swf@gmail.com>
2022-06-13 20:52:06 -0300

    Option: studying options

--- src/params/parameter-selector.ts
+++ src/params/parameter-selector.ts
@@ -3,15 +3,23 @@ import { KanaPanel } from './kana-panel';
 import { OptionsPanel } from './options-panel';
 
 export class ParameterSelector {
-	private kanas: KanaPanel[];
-	private _data: Parameters;
+	private kanaPanels: KanaPanel[];
+	private optionsPanel: OptionsPanel;
 
-	public get canStart() { return this.kanas.every(p => p.hasEnoughSelection); }
-	public get data() { return this._data; }
+	private kanas: KanaModel[];
+
+	public get canStart() { return this.kanaPanels.every(p => p.hasEnoughSelection); }
+	public get data(): Parameters {
+		return {
+			studying: this.optionsPanel.studying,
+			time: this.optionsPanel.time,
+			kanas: this.kanas
+		}
+	}
 
 	public setup(parent: HTMLElement, kanas: KanaModel[]) {
-		this._data = { studying: 'writes', time: 0, kanas };
-		this.kanas = kanas.map(kana => new KanaPanel(parent, kana));
-		new OptionsPanel().setup(document.querySelector('#options') as HTMLElement, this._data);
+		this.kanas = kanas;
+		this.kanaPanels = kanas.map(kana => new KanaPanel(parent, kana));
+		this.optionsPanel = new OptionsPanel(document.querySelector('#options') as HTMLElement);
 	}
 }
