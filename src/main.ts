b6b08e5875533d06d7f39035ac3730fdca26c12b
Kleber Silva <kleber@jogoglobal.com>
2022-06-13 12:30:02 -0300

    Using scss instead of css

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { ContentSelector } from './content-selector';
 import hiragana from '../content/hiragana.json';
 
 const contents: any[] = [hiragana];
-
 const selector = new ContentSelector();
 
 Promise.all(contents.map(e => fetch(e)))
 	.then(responses => Promise.all(responses.map(e => e.json())))
 	.then(contents => {
-		selector.setup(document.querySelector('#content-selector')!, contents);
+		selector.setup(document.querySelector('#selector')!, contents);
 	})
 	.catch(console.error);
 
