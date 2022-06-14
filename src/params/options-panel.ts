4191009a981075892ce87d3c648b8e9a0c9a70ff
Kleber Silva <kleber.swf@gmail.com>
2022-06-14 10:58:24 -0300

    Added NumberInput custom component

--- src/params/options-panel.ts
+++ src/params/options-panel.ts
@@ -1,14 +1,27 @@
 import { Study } from '../model';
+import { NumberInput } from '../number-input';
+
+const LABEL_DICT: Record<Study, string> = { reads: 'Read', writes: 'Write' };
 
 export class OptionsPanel {
-	private readonly timeElement: HTMLInputElement;
-	private readonly studyingElement: HTMLSelectElement;
+	private readonly timeElement: NumberInput;
+	private _studying: Study = 'writes';
 
-	public get time() { return parseInt(this.timeElement.value) * 1000; }
-	public get studying() { return this.studyingElement.value as Study; }
+	public get time() { return this.timeElement.value * 1000; }
+
+	public get studying() { return this._studying; }
 
 	public constructor(panel: HTMLElement) {
-		this.timeElement = panel.querySelector('#time') as HTMLInputElement;
-		this.studyingElement = panel.querySelector('#studying') as HTMLSelectElement;
+		this.timeElement = panel.querySelector('#time') as NumberInput;
+
+		const studying = panel.querySelector('#studying') as HTMLElement;
+		studying.innerText = LABEL_DICT[this._studying];
+		studying.addEventListener('click', this.toggleStudying.bind(this));
+	}
+
+	private toggleStudying(e: Event) {
+		const el = (e.target as HTMLElement);
+		this._studying = this._studying === 'reads' ? 'writes' : 'reads';
+		el.innerText = LABEL_DICT[this._studying];
 	}
 }
