965d266aa13a9ca96265430b5a9342bbe060cace
Kleber Silva <kleber.swf@gmail.com>
2022-06-14 12:19:00 -0300

    error styling

--- src/error.ts
+++ src/error.ts
@@ -1,15 +1,29 @@
 const ERROR_TIMEOUT = 2000;
 const VISIBLE_CLASS = 'visible';
 
-const error = document.querySelector('#error')! as HTMLElement;
-let tid: any;
 
-export function showError(message: string) {
-	error.innerText = message;
-	error.classList.add(VISIBLE_CLASS);
-	clearTimeout(tid);
-	tid = setTimeout(() => {
-		tid = null;
-		error.classList.remove(VISIBLE_CLASS);
-	}, ERROR_TIMEOUT);
+export class ErrorHandler {
+	private tid: any;
+	private element: HTMLElement;
+
+	constructor() {
+		this.element = document.querySelector('#error')! as HTMLElement;
+	}
+
+	public show(message: string) {
+		this.element.innerText = message;
+		this.element.classList.add(VISIBLE_CLASS);
+		clearTimeout(this.tid);
+		this.tid = setTimeout(() => {
+			this.tid = null;
+			this.element.classList.remove(VISIBLE_CLASS);
+		}, ERROR_TIMEOUT);
+	}
+
+	public hide() {
+		clearTimeout(this.tid);
+		this.tid = null;
+		this.element.classList.remove(VISIBLE_CLASS);
+	}
 }
+
