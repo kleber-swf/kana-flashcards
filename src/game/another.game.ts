f7ace5686542645b7e36079c69c3c61e15903d80
Kleber Silva <kleber.swf@gmail.com>
2022-06-15 07:49:28 -0300

    Parameterized timeline

--- src/game/another.game.ts
+++ src/game/another.game.ts
@@ -1,26 +1,35 @@
 import gsap from 'gsap';
-import { CharacterModel, Parameters } from '../model';
+import { CharacterModel, Parameters, Study } from '../model';
+
+const ACTION_KEY = 'Space';
 
 export class AnotherGame extends HTMLElement {
 	private readonly kana: HTMLElement;
 	private readonly romaji: HTMLElement;
+	private readonly timelineParams: GSAPTimelineVars;
 
 	private chars: CharacterModel[];
+	private hasTime: boolean;
 
+	private timeline: GSAPTimeline;
 	private selectedCharIndex: number;
-	private canAdvance: boolean;
 
 	constructor() {
 		super();
 
-		this.kana = this.appendChild(document.createElement('div'));
-		this.kana.classList.add('kana');
+		const parent = this.appendChild(document.createElement('div'));
+		parent.classList.add('result');
 
-		this.romaji = this.appendChild(document.createElement('div'));
+		this.romaji = parent.appendChild(document.createElement('div'));
 		this.romaji.classList.add('romaji');
 
-		this.canAdvance = true;
+		this.kana = parent.appendChild(document.createElement('div'));
+		this.kana.classList.add('kana');
+
+		this.timelineParams = { onComplete: this.onTimelineComplete.bind(this) };
+
 		document.addEventListener('click', this.onClick.bind(this));
+		document.addEventListener('keyup', this.onKeyUp.bind(this));
 	}
 
 	public start(params: Parameters) {
@@ -28,6 +37,26 @@ export class AnotherGame extends HTMLElement {
 			.map(k => k.groups).flat()
 			.map(g => g.characters).flat()
 			.filter(c => c && !c.hidden);
+		this.createTimeline(params.studying, params.time);
+		this.nextCharacter();
+	}
+
+	private createTimeline(studying: Study, time: number) {
+		this.hasTime = !isNaN(time) && time > 0;
+		const chars = studying === 'reads' ? [this.kana, this.romaji] : [this.romaji, this.kana];
+
+		this.timeline = gsap.timeline({ paused: true })
+			.to(chars, { opacity: 0, duration: 0.5 })
+			.call(() => this.randomCharacter())
+			.addLabel('step1')
+			.to(chars[0], { opacity: 1, duration: 0.5 })
+			.addLabel('step2');
+
+		if (this.hasTime) {
+			this.timeline.set({}, { delay: time }).addLabel('step3')
+		}
+
+		this.timeline.to(chars[1], { opacity: 1, duration: 0.5 });
 	}
 
 	private randomCharacter() {
@@ -35,23 +64,43 @@ export class AnotherGame extends HTMLElement {
 		do i = Math.floor(Math.random() * this.chars.length);
 		while (i === this.selectedCharIndex);
 		this.selectedCharIndex = i;
-		return this.chars[i];
+		const char = this.chars[i];
+		this.romaji.innerText = char.reads;
+		this.kana.innerText = char.writes;
 	}
 
 	private nextCharacter() {
-		if (!this.canAdvance) return;
-		const char = this.randomCharacter();
-		this.canAdvance = false;
-		const { romaji, kana } = this;
-		const timeline = gsap.timeline({ onComplete: () => { this.canAdvance = true } });
-		timeline.add(gsap.to([romaji, kana], { opacity: 0 }));
-		timeline.add(gsap.set(romaji, { innerText: char.reads }));
-		timeline.add(gsap.set(kana, { innerText: char.writes }));
-		timeline.add(gsap.to(romaji, { opacity: 1 }).delay(1));
-		timeline.add(gsap.to(kana, { opacity: 1 }).delay(1));
+		this.timeline.restart();
+		if (!this.hasTime) {
+			this.timeline.tweenFromTo(0, 'step2');
+		}
+	}
+
+
+	private nextStep() {
+		if (this.timeline.progress() === 1) {
+			this.nextCharacter();
+			return;
+		}
+
+		if (!this.timeline.currentLabel()) return;
+
+		const nextLabel = this.timeline.nextLabel();
+		if (nextLabel) this.timeline.play(nextLabel);
+		else this.timeline.resume();
+	}
+
+	private onTimelineComplete() {
+		console.log(this.timeline);
 	}
 
 	private onClick() {
-		this.nextCharacter();
+		this.nextStep();
+	}
+
+	private onKeyUp(e: KeyboardEvent) {
+		if (e.code === ACTION_KEY) {
+			this.nextStep();
+		}
 	}
 }
