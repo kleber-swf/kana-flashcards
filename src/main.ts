71ecb228eccb537a202016a0f9f191f68658fb1e
Kleber Silva <kleber@jogoglobal.com>
2022-06-17 18:30:39 -0300

    Saving/loading preferences with `localStorage`

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { Game } from './game/game';
 import { KanaPanel } from './params/kana-panel';
 import { ParameterSelector } from './params/parameter-selector';
 
+const LS_KEY = 'params';
+
 customElements.define('number-input', NumberInput);
 customElements.define('text-toggle', TextToggle);
 customElements.define('kana-panel', KanaPanel);
@@ -17,16 +19,22 @@ const parameters = new ParameterSelector();
 const game = document.querySelector('#game') as Game;
 const error = document.querySelector('#error') as ErrorSnackbar;
 
+const saved = window.localStorage.getItem(LS_KEY);
+
 Promise.all(([hiragana, katakana] as any[]).map(e => fetch(e)))
 	.then(responses => Promise.all(responses.filter(e => e.ok).map(e => e.json())))
-	.then(contents => parameters.setup(contents))
+	.then(contents => parameters.setup(contents, saved))
 	.catch(console.error);
+
 
 document.querySelector('#start-button')?.addEventListener('click', () => {
 	if (!parameters.canStart) {
 		error.show('Select at least two characters');
 		return;
 	}
+
+	const data = parameters.data;
 	error.hide();
-	game.start(parameters.data);
+	game.start(data);
+	window.localStorage.setItem(LS_KEY, JSON.stringify(data));
 });
--- src/params/parameter-selector.ts
+++ src/params/parameter-selector.ts
@@ -1,3 +1,4 @@
+import merge from 'deepmerge-json';
 import { KanaModel, Parameters } from '../model';
 import { KanaPanel } from './kana-panel';
 import { OptionsPanel } from './options-panel';
@@ -21,17 +22,26 @@ export class ParameterSelector {
 		};
 	}
 
-	public setup(kanas: KanaModel[]) {
-		this.kanas = kanas;
+	public setup(kanas: KanaModel[], saved: string | null) {
 		const kanaParent = document.querySelector('#kanas') as HTMLElement;
+		const initialData = this.getInitialData(kanas, saved);
+		console.log(initialData);
+		this.kanas = initialData.kanas;
 
-		this.kanaPanels = kanas.map(kana => {
+		this.kanaPanels = initialData.kanas.map(kana => {
 			const panel = document.createElement('kana-panel') as KanaPanel;
 			kanaParent.appendChild(panel);
 			panel.setup(kana);
 			return panel;
 		});
 
-		this.optionsPanel = new OptionsPanel(document.querySelector('#options') as HTMLElement);
+		this.optionsPanel = new OptionsPanel(
+			document.querySelector('#options') as HTMLElement,
+			initialData);
+	}
+
+	private getInitialData(kanas: KanaModel[], saved: string | null): Parameters {
+		const params: Parameters = { training: 'writes', revealDelay: 0, autoAdvanceDelay: 0, kanas, };
+		return saved ? merge(params, JSON.parse(saved)) : params;
 	}
 }
