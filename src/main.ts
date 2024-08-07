536441155d6ad6b680a56bd032c820081893ae3d
Kleber Silva <kleber.swf@gmail.com>
2024-08-07 14:04:45 -0300

    error styling

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
 import hiragana from '../content/hiragana.json';
 import { NumberInput } from './components/number-input';
 import { TextToggle } from './components/text-toggle';
-import { showError } from './error';
+import { ErrorHandler } from './error';
 import { Game } from './game/game';
 import { ParameterSelector } from './params/parameter-selector';
 
 const parameters = new ParameterSelector();
+const error = new ErrorHandler();
 const game = new Game();
 
 customElements.define('number-input', NumberInput);
 customElements.define('text-toggle', TextToggle);
 
-document.querySelector('#start-button')!
-	.addEventListener('click', () => {
-		if (parameters.canStart) game.show(parameters.data);
-		else showError('Select at least two characters');
-	});
+function tryStartGame() {
+	if (!parameters.canStart) {
+		error.show('Select at least two characters');
+		return;
+	}
+	error.hide();
+	game.show(parameters.data);
+}
+
+document.querySelector('#start-button')!.addEventListener('click', tryStartGame);
 
 Promise.all(([hiragana] as any[]).map(e => fetch(e)))
 	.then(responses => Promise.all(responses.filter(e => e.ok).map(e => e.json())))
