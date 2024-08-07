252dd37b1dc42e4cd7aca9cd1a69cbeb25f1f781
Kleber Silva <kleber.swf@gmail.com>
2024-08-07 14:05:51 -0300

    Added: Script to convert audio files to a module

---
+++ scripts/audio-files-to-module.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const IN_DIR = './audio';
+const EXT = '.mp3';
+const EXT_LEN = EXT.length;
+
+const OUT_FILE = './src/audio.ts';
+const OUT_HEADER = 'export const AUDIO = {';
+const OUT_FOOTER = '};';
+
+const files = (await fs.readdir(IN_DIR,))
+	.filter(e => e.endsWith(EXT))
+	.map(e => `	'${e.substring(0, e.length - EXT_LEN)}': require('../audio/${e}'),`)
+	.join('\n');
+
+try {
+	const content = `${OUT_HEADER}\n${files}\n${OUT_FOOTER}\n`;
+	await fs.writeFile(OUT_FILE, content);
+	console.log(`Successfully created file: ${OUT_FILE}`);
+} catch (e) {
+	console.error(`Error trying to save output file at ${OUT_FILE}`);
+	console.error(e);
+}
+
