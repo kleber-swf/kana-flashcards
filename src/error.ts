21b6ddb027559125b176d13327fb43addae5d2cd
Kleber Silva <kleber.swf@gmail.com>
2024-08-07 14:04:45 -0300

    It's possible to unterrupt the timer

--- src/error.ts
+++ src/error.ts
@@ -2,14 +2,14 @@ const ERROR_TIMEOUT = 2000;
 const VISIBLE_CLASS = 'visible';
 
 const error = document.querySelector('#error')! as HTMLElement;
-let timeout: any;
+let tid: any;
 
 export function showError(message: string) {
 	error.innerText = message;
 	error.classList.add(VISIBLE_CLASS);
-	if (timeout) clearTimeout(timeout);
-	timeout = setTimeout(() => {
-		timeout = null;
+	clearTimeout(tid);
+	tid = setTimeout(() => {
+		tid = null;
 		error.classList.remove(VISIBLE_CLASS);
 	}, ERROR_TIMEOUT);
 }
